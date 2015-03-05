db1bb7cc7d6b6ab090f0781c730ba53ca16b3381
Damian Jaszczurowski <damian.jaszczurowski@gmail.com>
2015-03-05 19:30:20 +0100

    Move all js code into anonymous self-invoking function. Pass dispatcher as in props

--- app/assets/javascripts/pages/comments.js
+++ app/assets/javascripts/pages/comments.js
@@ -1,119 +1,123 @@
-/** @jsx React.DOM */
-var markdownConverter = new Showdown.converter();
+(function() {
+  /** @jsx React.DOM */
+  var markdownConverter = new Showdown.converter();
 
-var Comment = React.createClass({
-  render: function() {
-    var contentMarkdown = markdownConverter.makeHtml((this.props.children || '').toString())
+  var Comment = React.createClass({
+    render: function() {
+      var contentMarkdown = markdownConverter.makeHtml((this.props.children || '').toString())
 
-    return (
-      <div className="comment">
-        <span>
-          <b>{this.props.author}: </b>
-        </span>
-
-        <span>
-          <em>{this.props.content}</em>
-        </span>
-
-        <span dangerouslySetInnerHTML={{__html: contentMarkdown}} />
-      </div>
-    );
-  }
-});
-
-var CommentList = React.createClass({
-  render: function() {
-    var commentNodes = this.props.data.map(function(dataItem) {
       return (
-        <Comment author={dataItem.author} content={dataItem.content}>
-          {dataItem.markdown || ''}
-        </Comment>
-      )
-    });
+        <div className="comment">
+          <span>
+            <b>{this.props.author}: </b>
+          </span>
 
-    return (
-      <div className="commentList">
-        {commentNodes}
-      </div>
-    );
-  }
-});
+          <span>
+            <em>{this.props.content}</em>
+          </span>
 
-var CommentForm = React.createClass({
-  _handleSubmit: function(event) {
-    event.preventDefault();
-
-    var comment = {
-      author: this.refs.author.getDOMNode().value.trim(),
-      content: this.refs.content.getDOMNode().value.trim(),
-      markdown: this.refs.markdown.getDOMNode().value.trim(),
+          <span dangerouslySetInnerHTML={{__html: contentMarkdown}} />
+        </div>
+      );
     }
+  });
 
-    var onSuccess = function(comment) {};
-    var onFailure = function(comment) { console.log("comment creations failure"); };
+  var CommentList = React.createClass({
+    render: function() {
+      var commentNodes = this.props.data.map(function(dataItem) {
+        return (
+          <Comment author={dataItem.author} content={dataItem.content}>
+            {dataItem.markdown || ''}
+          </Comment>
+        )
+      });
 
-    window._dispatcher.trigger("create_comment", {comment: comment}, onSuccess, onFailure);
-  },
-  render: function() {
-    return (
-      <form className="commentForm" onSubmit={this._handleSubmit}>
-        <input type="text" placeholder="Your name" ref="author"/>
-        <input type="text" placeholder="Content of your comment" ref="content" />
-        <input type="text" placeholder="markdown (optional)" ref="markdown"/>
-        <input type="submit" value="Post" />
-      </form>
-    )
-  }
-});
+      return (
+        <div className="commentList">
+          {commentNodes}
+        </div>
+      );
+    }
+  });
 
-var CommentBox = React.createClass({
-  _fetchInitComments: function() {
-    var that = this;
+  var CommentForm = React.createClass({
+    _handleSubmitForm: function(event) {
+      event.preventDefault();
 
-    $.ajax(
-      {
-        url: this.props.url,
-        dataType: 'json',
-        success: function(data) {
-          var newState = that.state;
-          newState.data = data;
-
-          that.setState(newState);
-        },
-        error: function() {
-          console.log("error")
-        }
+      var comment = {
+        author: this.refs.author.getDOMNode().value.trim(),
+        content: this.refs.content.getDOMNode().value.trim(),
+        markdown: this.refs.markdown.getDOMNode().value.trim(),
       }
-    )
-  },
-  _bindDispather: function() {
-    var commentsChannel = window._dispatcher.subscribe("comments");
-    var that = this;
 
-    commentsChannel.bind("created", function(comment) {
-      var newState = that.state;
-      newState.data.push(comment);
+      var onSuccess = function(comment) {};
+      var onFailure = function(comment) { console.log("comment creations failure"); };
 
-      that.setState(newState);
-    });
-  },
-  getInitialState: function() {
-    return { data: [] }
-  },
-  componentDidMount: function() {
-    this._fetchInitComments();
-    this._bindDispather();
-  },
-  render: function() {
-    return (
-      <div className="commentBox">
-        <CommentList data={this.state.data}/>
-        Enter new comment:
-        <CommentForm />
-      </div>
-    );
-  }
-});
+      this._owner.state.dispatcher.trigger("create_comment", {comment: comment}, onSuccess, onFailure);
+    },
+    render: function() {
+      return (
+        <form className="commentForm" onSubmit={this._handleSubmitForm}>
+          <input type="text" placeholder="Your name" ref="author"/>
+          <input type="text" placeholder="Content of your comment" ref="content" />
+          <input type="text" placeholder="markdown (optional)" ref="markdown"/>
+          <input type="submit" value="Post" />
+        </form>
+      )
+    }
+  });
 
-window._dispatcher = new WebSocketRails("localhost:3000/websocket")
-React.render(<CommentBox url="api/comments" />, document.getElementById("content"))
+  var CommentBox = React.createClass({
+    _fetchInitComments: function() {
+      var that = this;
+
+      $.ajax(
+        {
+          url: this.props.url,
+          dataType: 'json',
+          success: function(comments) {
+            that.setState({data: comments});
+          },
+          error: function() { console.log("error") }
+        }
+      )
+    },
+    _bindDispather: function() {
+      var commentsChannel = this.state.dispatcher.subscribe("comments");
+      var that = this;
+
+      commentsChannel.bind("created", function(comment) {
+        var newState = that.state.data;
+        newState.push(comment);
+
+        that.setState(newState);
+      });
+    },
+    getInitialState: function() {
+      return {
+        data: [],
+        dispatcher: this.props.dispatcher
+      }
+    },
+    componentDidMount: function() {
+      this._fetchInitComments();
+      this._bindDispather();
+    },
+    render: function() {
+      return (
+        <div className="commentBox">
+          <CommentList data={this.state.data}/>
+          Enter new comment:
+          <CommentForm />
+        </div>
+      );
+    }
+  });
+
+  var dispatcher = new WebSocketRails("localhost:3000/websocket");
+
+  React.render(
+    <CommentBox dispatcher={dispatcher} url="api/comments" />,
+    document.getElementById("content")
+  )
+}());
